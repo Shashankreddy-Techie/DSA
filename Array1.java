6eb7c6bd5bd86a78a25c2664ebe05f56f2d05bf5
Shashankreddy-Techie <152247010+Shashankreddy-Techie@users.noreply.github.com>
2024-08-01 16:57:40 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Array1 {
+    public static void main(String[] args) {
+        //Syntax//
+        // datatype[] var_name= new datatype[size];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the element at index "+i+" :");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
