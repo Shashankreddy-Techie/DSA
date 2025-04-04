6d3803b0213ade869f8ba85d99530cd282c640c1
NUKALA SHASHANK REDDY <152247010+Shashankreddy-Techie@users.noreply.github.com>
2025-04-04 21:56:37 +0530

    Create traverseInLinkedList.java

---
+++ traverseInLinkedList.java
@@ -0,0 +1,74 @@
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    Node head;
+
+    // Insert a new node at the end of the list
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    // Print the list from head to tail
+    public void printListForward() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    // Print the list from tail to head
+    public void printListBackward() {
+        Node temp = head;
+        if (temp == null) return;
+
+        // Move to the last node
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        // Traverse backward
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insert(10);
+        dll.insert(20);
+        dll.insert(30);
+        dll.insert(40);
+
+        System.out.print("List in forward direction: ");
+        dll.printListForward(); // Output: 10 20 30 40
+
+        System.out.print("List in backward direction: ");
+        dll.printListBackward(); // Output: 40 30 20 10
+    }
+}
