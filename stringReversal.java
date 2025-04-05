00c52cbf08005c4ebe143b20b03e46e07c2c59f2
NUKALA SHASHANK REDDY <152247010+Shashankreddy-Techie@users.noreply.github.com>
2025-04-05 09:59:29 +0530

    Create stringReversal.java

---
+++ stringReversal.java
@@ -0,0 +1,13 @@
+public class StringReversal {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverseString(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder(str);
+        return reversed.reverse().toString();
+    }
+}
