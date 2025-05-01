c61e97e9f27e21223b5695a2b4f41b446ddb00bf
NUKALA SHASHANK REDDY <152247010+Shashankreddy-Techie@users.noreply.github.com>
2025-05-01 08:02:20 +0530

    Create inOrder.java

---
+++ inOrder.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTree {
+    
+    // Inorder Traversal
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderHelper(root, result);
+        return result;
+    }
+
+    private void inorderHelper(TreeNode node, List<Integer> result) {
+        if (node != null) {
+            inorderHelper(node.left, result);
+            result.add(node.val);
+            inorderHelper(node.right, result);
+        }
+    }
+}
